8b3536304ab6c29fe471b4d6e1b7572cade25a53
Gautam Gupta <ggupta78@gmail.com>
2022-11-10 14:31:12 +0800

    complete models setup

---
+++ app/models/bookmark.rb
@@ -0,0 +1,10 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :comment, length: { minimum: 6 }
+  validates :movie, uniqueness: {
+    scope: :list,
+    message: 'this list and movie combination already exists'
+  }
+end
