4c0fe5f169b69e3e98279a7cef2dbcf24db25bd7
Gautam Gupta <ggupta78@gmail.com>
2022-11-10 17:50:40 +0800

    complete bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,35 @@
 class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+    @movies = Movie.all
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    saved = @bookmark.save
+
+    if saved
+      redirect_to list_path(@bookmark.list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = List.find(params[:list_id])
+
+    @bookmark.destroy
+
+    redirect_to list_path(@list), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 end
